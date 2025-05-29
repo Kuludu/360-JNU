d7815784bc26d40f70465813f532a8bd5187e391
Yuxiang Fan <opensource@kuludu.net>
2025-05-29 23:08:40 +0800

    Add compressed image & fix the script.

--- script/compress.py
+++ script/compress.py
@@ -11,7 +11,7 @@ for filename in os.listdir(image_dir):
         compressed_file_path = os.path.join(image_dir, compressed_filename)
         try:
             img = Image.open(file_path)
-            img.save(compressed_file_path, quality=60, optimize=True)
+            img.save(compressed_file_path, quality=40, optimize=True)
             print(f"压缩完成: {compressed_filename}")
         except Exception as e:
             print(f"压缩失败: {filename}, 错误: {e}")
